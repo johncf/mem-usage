3ed5737eeebac138dbe40684c49c4a18e3d0a807
John C <jcharankattu@cogent.co.jp>
2019-04-16 16:00:22 +0900

    for-pid.py: monitor vms

--- for-pid.py
+++ for-pid.py
@@ -7,25 +7,35 @@ from sys import argv, stdout
 
 
 def main(pid: int):
-    """prints epoch-time, number of processes, and RSS (in bytes) once every second"""
+    """prints epoch-time, number of processes, RSS (bytes), and VMS (bytes) once every second"""
     p = psutil.Process(pid)
     while True:
         rss = get_rss(p)
+        vms = get_vms(p)
         if not rss:
             break
         num_proc = 1
         for c in p.children(True):
             rss += get_rss(c)
+            vms += get_vms(c)
             num_proc += 1
-        print(f"{int(time.time())} {num_proc} {rss}")
+        print(f"{int(time.time())} {num_proc} {rss} {vms}")
         time.sleep(1)
         if random.randint(1, 20) == 1:
             stdout.flush()
 
 
 def get_rss(proc):
+    return get_prop(proc, 'rss')
+
+
+def get_vms(proc):
+    return get_prop(proc, 'vms')
+
+
+def get_prop(proc, prop):
     try:
-        return proc.memory_info().rss
+        return getattr(proc.memory_info(), prop, 0)
     except psutil.NoSuchProcess:
         return 0
 
